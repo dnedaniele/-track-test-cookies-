94f8855064533cddce93c9910e4a7822cee10055
Daniele Esposito <esp.dne@gmail.com>
2020-11-12 16:04:50 +0100

    cookies delete

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-function sendCookie () {
-    document.cookie = "daniele_cookies = 123456"
-    console.log(document.cookie)
+
+function generateId() {
+    return parseInt(Math.random() * 1000000); // 123123123123123
+  }
+
+  
+
+  function sendCookie () {
+
+        const cookieValue = document.cookie
+          .split("; ")
+          .find((row) => row.startsWith("user_id")); // "user=123123"
+
+          let userId = null;
+    // New User
+        if (cookieValue === null || cookieValue === undefined) {
+
+          userId = generateId();
+          document.cookie = "user_id=" + userId;
+          let infoNewUser = " no cookie here - This is a New User, we add the cookies " + document.cookie;
+          document.querySelector(".show-cookies").textContent = infoNewUser;
+        }else{
+            userId = cookieValue.split("=")[1];
+            let infoOldUser = "   This is an Old User - existing cookie: user_id = " + userId;
+            document.querySelector(".show-cookies").textContent = infoOldUser;
+        }
+        return userId;
 }
+
+
+function deleteCookie() {
+    document.cookie = "user_id = deleteMe; expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+}
+  
